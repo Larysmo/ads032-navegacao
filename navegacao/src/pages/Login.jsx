d1e050f3c9a78095729006a9ab136da152362cc6
Jose Reginaldo de Sousa Junior <3056@iesb.br>
2023-06-02 19:50:11 -0300

    formulario e contexto

--- navegacao/src/pages/Login.jsx
+++ navegacao/src/pages/Login.jsx
@@ -1,17 +1,14 @@
-import { useNavigate } from "react-router-dom";
+// import LoginForm from "./LoginForm";
+import LoginRHF from "./LoginRHF";
+import '../App.css'
+
 
 export default function Login(props) {
-    const navigate = useNavigate();
-
-    function handleClick(event) {
-        props.onLogin(event);
-        navigate("/");
-    }
-
     return (
         <>
             <h1>Login</h1>
-            <button onClick={handleClick}>Entrar</button>
+            {/* <LoginForm onSubmit={handleClick} /> */}
+            <LoginRHF />
         </>
     )
 }
